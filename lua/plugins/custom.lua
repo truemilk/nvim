0b3ebf9d01d785fc306e1c15f74286452c71c186
Ivan Giacomelli <ivan@nopsled.it>
2026-03-13 13:12:39 +0200

    init

---
+++ lua/plugins/custom.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "mikavilpas/yazi.nvim",
+    version = "*",
+    event = "VeryLazy",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", lazy = true },
+    },
+    keys = {
+      {
+        "<leader>yy",
+        mode = { "n", "v" },
+        "<cmd>Yazi<cr>",
+        desc = "Open yazi at the current file",
+      },
+    },
+    opts = {
+      open_for_directories = false,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+    init = function()
+      vim.g.loaded_netrwPlugin = 1
+    end,
+  },
+}
