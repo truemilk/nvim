a44a842bca0c6bb949774c79e45465eb7837ab38
Ivan Giacomelli <ivan@nopsled.it>
2026-03-21 13:27:37 +0200

    updates

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,48 +1,48 @@
 return {
-  {
-    "folke/tokyonight.nvim",
-    opts = { style = "night" },
-  },
-  {
-    "folke/snacks.nvim",
-    opts = {
-      dashboard = { enabled = false },
-      picker = {
-        sources = {
-          files = { hidden = true, ignored = false },
-          grep = { hidden = true, ignored = false },
-        },
-      },
-      terminal = {
-        win = { position = "float" },
-      },
-    },
-  },
-  {
-    "folke/noice.nvim",
-    opts = {
-      presets = { lsp_doc_border = true },
-      cmdline = { enabled = false },
-      messages = { enabled = false },
-    },
-  },
-  {
-    "saghen/blink.cmp",
-    opts = {
-      keymap = {
-        ["<CR>"] = {},
-        ["<C-y>"] = { "select_and_accept", "fallback" },
-      },
-      completion = {
-        menu = { border = "rounded" },
-        documentation = { window = { border = "rounded" } },
-      },
-    },
-  },
-  {
-    "folke/which-key.nvim",
-    opts = {
-      preset = "classic",
-    },
-  },
+	{
+		"folke/tokyonight.nvim",
+		opts = { style = "night" },
+	},
+	{
+		"folke/snacks.nvim",
+		opts = {
+			dashboard = { enabled = false },
+			--		picker = {
+			--			sources = {
+			--				files = { hidden = true, ignored = false },
+			--				grep = { hidden = true, ignored = false },
+			--			},
+			--		},
+			--		--terminal = {
+			--		--	win = { position = "float" },
+			--		--},
+		},
+	},
+	{
+		"folke/noice.nvim",
+		opts = {
+			presets = { lsp_doc_border = true },
+			cmdline = { enabled = false },
+			messages = { enabled = false },
+		},
+	},
+	{
+		"saghen/blink.cmp",
+		opts = {
+			keymap = {
+				["<CR>"] = {},
+				["<C-y>"] = { "select_and_accept", "fallback" },
+			},
+			completion = {
+				menu = { border = "rounded" },
+				documentation = { window = { border = "rounded" } },
+			},
+		},
+	},
+	--{
+	--  "folke/which-key.nvim",
+	--  opts = {
+	--    preset = "classic",
+	--  },
+	--},
 }
--- lua/plugins/custom.lua
+++ lua/plugins/custom.lua
@@ -1,27 +1,43 @@
+if true then
+	return {}
+end
+
 return {
-  {
-    "mikavilpas/yazi.nvim",
-    version = "*",
-    event = "VeryLazy",
-    dependencies = {
-      { "nvim-lua/plenary.nvim", lazy = true },
-    },
-    keys = {
-      {
-        "<leader>yy",
-        mode = { "n", "v" },
-        "<cmd>Yazi<cr>",
-        desc = "Open yazi at the current file",
-      },
-    },
-    opts = {
-      open_for_directories = false,
-      keymaps = {
-        show_help = "<f1>",
-      },
-    },
-    init = function()
-      vim.g.loaded_netrwPlugin = 1
-    end,
-  },
+	{
+		"mikavilpas/yazi.nvim",
+		version = "*",
+		event = "VeryLazy",
+		dependencies = {
+			{ "nvim-lua/plenary.nvim", lazy = true },
+		},
+		keys = {
+			{
+				"<leader>yy",
+				mode = { "n", "v" },
+				"<cmd>Yazi<cr>",
+				desc = "Open yazi at the current file",
+			},
+		},
+		opts = {
+			open_for_directories = false,
+			keymaps = {
+				show_help = "<f1>",
+			},
+		},
+		init = function()
+			vim.g.loaded_netrwPlugin = 1
+		end,
+	},
+	{
+		"vague-theme/vague.nvim",
+		lazy = false, -- make sure we load this during startup if it is your main colorscheme
+		priority = 1000, -- make sure to load this before all the other plugins
+		config = function()
+			-- NOTE: you do not need to call setup if you don't want to.
+			require("vague").setup({
+				-- optional configuration here
+			})
+			vim.cmd("colorscheme vague")
+		end,
+	},
 }
