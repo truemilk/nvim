fa2c8fb74c91fc9e8c2234c1bf0bab2b81a59e67
Ivan Giacomelli <ivan@nopsled.it>
2026-04-04 20:51:25 +0300

    updates

--- lua/plugins/core.lua
+++ lua/plugins/core.lua
@@ -1,8 +1,8 @@
 return {
-	{
-		"folke/tokyonight.nvim",
-		opts = { style = "night" },
-	},
+	--{
+	--	"folke/tokyonight.nvim",
+	--	opts = { style = "night" },
+	--},
 	--{
 	--	"folke/snacks.nvim",
 	--	opts = {
